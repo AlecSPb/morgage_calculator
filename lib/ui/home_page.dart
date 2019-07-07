5d8a5f3d72aea7ed035456bd2d270cbb44795042
daimon <bugrov.dmitrij@gmail.com>
2019-07-07 23:35:52 +0300

    app bar style improved

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:road_keeper_mobile/ui/payments_list/payments_list_screen.dart';
 
+import '../constants.dart';
 import 'calculator/morgage_input_screen.dart';
 import 'custom_icons_icons.dart';
 
@@ -27,20 +28,49 @@ class _HomePageState extends State<HomePage> {
               headerSliverBuilder: (context, innerBoxIsScrolled) {
                 return [
                   SliverAppBar(
+                    backgroundColor: CustomThemeColors.backgroundColor,
                     elevation: 0,
                     centerTitle: true,
                     pinned: true,
                     floating: true,
-                    title: const Text('MortGage calculator'),
+                    title: const Text('MortGage calculator',
+                        style: TextStyle(
+                            inherit: true,
+                            color: CustomThemeColors.primaryColor)),
                     forceElevated: innerBoxIsScrolled,
-                    bottom: TabBar(tabs: [
-                      Tab(icon: Icon(CustomIcons.calc)),
-                      Tab(icon: Icon(Icons.view_list))
-                    ]),
+                    bottom: _getAppBar()
                   )
                 ];
               },
               body: TabBarView(children: _widgetOptions))),
+    );
+  }
+
+  Widget _getAppBar() {
+    var tabBar = TabBar(
+        indicatorColor: CustomThemeColors.primaryColor,
+        indicatorWeight: 3.0,
+        indicatorPadding: EdgeInsets.symmetric(horizontal: 16.0),
+        labelColor: CustomThemeColors.primaryColor,
+        unselectedLabelColor: CustomThemeColors.primaryLight,
+        tabs: [
+          Tab(
+            icon: Icon(CustomIcons.calc),
+          ),
+          Tab(icon: Icon(Icons.view_list))
+        ]);
+    return PreferredSize(
+      child: Stack(
+        children: [
+          Container(
+              height: tabBar.preferredSize.height,
+              decoration: BoxDecoration(border: Border(bottom: BorderSide(color: CustomThemeColors.primaryLight.withOpacity(0.5),width: 3))),
+              child: SizedBox.fromSize(size: tabBar.preferredSize,)
+          ),
+          tabBar
+        ],
+      ),
+      preferredSize: tabBar.preferredSize,
     );
   }
 }
